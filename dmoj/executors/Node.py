b09bd651f108dd77c478d5b9d9a240a41287c6c1
Doncho Minkov <DonchoMinkov@gmail.com>
2017-08-22 18:41:10 +0300

    Adding Node.js executor

---
+++ dmoj/executors/Node.py
@@ -0,0 +1,14 @@
+from .base_executor import ScriptExecutor
+
+class NodeExecutor(ScriptExecutor):
+    ext = '.js'
+    address_grace = 131079
+    name = 'Nodejs'
+    command = 'node'
+    test_program = '''
+        const readline = require('/libs/node_modules/readline-sync/lib/readline-sync.js');
+        console.log(readline.question());
+    '''
+
+    def get_cmdline(self):
+        return ['node', self._code]
